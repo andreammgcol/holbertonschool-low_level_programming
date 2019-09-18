f2c3864eb24a655f68fa95ab8c6d4e33f6554515
andreammgcol <amendez72@misena.edu.co>
2019-09-18 23:04:27 +0000

    c low_level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
 #!/bin/bash
-puts ("Programming is like building a multilingual puzzle);
+#include <stdio.h>
+
+/**
+ * main - prints exactly
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+    puts (\"Programming is like building a multilingual puzzle)
+    return (0);
+}
