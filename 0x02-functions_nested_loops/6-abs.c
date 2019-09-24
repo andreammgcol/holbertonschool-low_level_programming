8da7acf25fbe0b8fb0f929d1378a1f1bb6767ebe
andreammgcol <amendez72@misena.edu.co>
2019-09-24 20:51:33 +0000

    c_fuctions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _abs - that compute the absolute value.
+ * @a: number to be checked
+ * Return: a if the number is positive.
+ */
+int _abs(int a)
+{
+if (a < 0)
+{
+a = a * -1;
+return (a);
+}
+else
+{
+return (a);
+}
+}
