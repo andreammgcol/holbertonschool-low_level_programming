2951bd51d6147dd92541e052a5f60a70d7cceff1
andreammgcol <amendez72@misena.edu.co>
2019-09-24 14:28:58 +0000

    c_fuctions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int alp;
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+_putchar(alp);
+}
+_putchar('\n');
+return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Learning C - Functions, nested loops
+
