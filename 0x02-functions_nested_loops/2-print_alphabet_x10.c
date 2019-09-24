2dc488829621d5883116c6222ad0e33ed181fa9f
andreammgcol <amendez72@misena.edu.co>
2019-09-24 17:07:27 +0000

    c_fuctions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * void print_alphabet_x10 - function that prints 10 times the alphabet
+ *
+ * Return:
+ */
+void print_alphabet_x10(void);
+{
+int j = 0;
+while (j < 10)
+{
+int a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+j++;
+}
+return;
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,5 +2,6 @@
 #define HOLBERTON_H
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 #endif
