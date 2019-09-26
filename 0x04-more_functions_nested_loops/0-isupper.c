43ebffb562ab56d0225a7799e5214903a28f7949
andreammgcol <amendez72@misena.edu.co>
2019-09-26 20:26:40 +0000

    more functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,12 @@
 */
 int _isupper(int c)
 {
-if (c >= 'a' && c <= 'z')
+if (c >= 'A' && c <= 'Z')
 {
-return (0);
-}
-else if (c >= 'A' && c <= 'Z')
-{
-}
 return (1);
+}
+else if (c >= 'a' && c <= 'z')
+{
+}
+return (0);
 }
