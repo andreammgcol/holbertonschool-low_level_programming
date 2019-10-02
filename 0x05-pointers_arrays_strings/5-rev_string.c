adbfde689de9c30dd9a84cfe2d43fe9608594f47
andreammgcol <amendez72@misena.edu.co>
2019-10-02 04:37:18 +0000

    pointers, arrays and string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+* rev_string -  that reverses a string
+* @s: variable pointer
+* Return:
+*/
+void rev_string(char *s)
+{
+int tam = 0;
+int j = 0;
+int i;
+int x;
+char m[tam];
+
+while (s[tam] != '\0')
+{
+tam++;
+}
+
+for (i = tam - 1; i >= 0; i--)
+{
+m[j] = s[i];
+j++;
+}
+
+for(x =0; x < tam; x++)
+{
+s[x] = m[x];
+}
+}
