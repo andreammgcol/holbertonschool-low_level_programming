2d6b946d87a454a49bd0f546147a9c4a9b5daa6d
andreammgcol <amendez72@misena.edu.co>
2019-10-03 15:22:30 +0000

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,11 +6,18 @@
 */
 void puts_half(char *str)
 {
-int n;
-for (n = 0; str[n] != '\0'; n++)
-{
-if (n % 2 != 0)
-_putchar(str[n]);
-}
-_putchar('\n');
+	int cont;
+
+	for (cont = 0; str[cont] != '\0'; cont++)
+	{
+	}
+	cont++;
+	cont = cont / 2;
+
+	while (str[cont] != '\0')
+	{
+	_putchar(str[cont]);
+	cont++;
+	}
+	_putchar('\n');
 }
