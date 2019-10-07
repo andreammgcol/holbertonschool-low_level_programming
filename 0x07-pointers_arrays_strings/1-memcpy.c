fc6d41e17121c0bc2ddaac5ff6550e4a13d8bcf9
andreammgcol <amendez72@misena.edu.co>
2019-10-07 19:29:43 +0000

    pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+* *_memcpy - that copies memory area
+* @dest: variable 1
+* @src: variable 2
+* @n: bytes
+* Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n && src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
