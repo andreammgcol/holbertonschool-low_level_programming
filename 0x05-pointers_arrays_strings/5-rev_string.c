5d9f8d3aad0ff11d623cdbaa7d2d1eefc7632f3b
andreammgcol <amendez72@misena.edu.co>
2019-10-07 01:34:32 +0000

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,28 +4,23 @@
 * @s: variable pointer
 * Return:
 */
+
 void rev_string(char *s)
 {
-int tam = 0;
-int j = 0;
-int i;
-int x;
-char m[] = "          ";
+	int tam = 0;
+	int i;
+	char temp;
 
-while (s[tam] != '\0')
-{
-tam++;
-}
+	while (s[tam] != '\0')
+	{
+		tam++;
+	}
 
-for (i = tam - 1; i >= 0; i--)
-{
-m[j] = s[i];
-j++;
-}
-
-for (x = 0; x < tam; x++)
-{
-s[x] = m[x];
-}
-s[tam] = '\0';
+	for (i = 0; i < tam; i++)
+	{
+		temp = s[i];
+		s[i] = s[tam - 1];
+		s[tam - 1] = temp;
+		tam--;
+	}
 }
