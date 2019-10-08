f6088c22250a718b51cf8df5e01fd50388140edd
andreammgcol <amendez72@misena.edu.co>
2019-10-08 02:52:40 +0000

    pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+* *_strstr - function that locates a substring
+* @haystack: variable pointer 1
+* @needle: variable pointer 2
+* Return: substring
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k = 0, counter = 0;
+	int worldLength = 0;
+
+	while (needle[k] != '\0')
+	{
+		worldLength++;
+		k++;
+	}
+
+	for (i = 0; haystack[i] != '\0' ; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				counter++;
+				i++;
+			}
+		}
+
+		if (worldLength == counter)
+		{
+			haystack = haystack + (i - worldLength);
+			return (haystack);
+		}
+		else
+		{
+			counter = 0;
+		}
+	}
+	haystack = haystack + 0;
+	return (haystack);
+}
