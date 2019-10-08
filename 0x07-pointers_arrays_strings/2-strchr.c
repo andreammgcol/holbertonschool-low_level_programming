80227df21f24fdf0c4eaf0b48ffbef1a5a18d92d
andreammgcol <amendez72@misena.edu.co>
2019-10-08 03:24:26 +0000

    pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,14 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-		return (s + i);
+			return (s + i);
 		}
-		i++;
 	}
 	return ('\0');
-
 }
