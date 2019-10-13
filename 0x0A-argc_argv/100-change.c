cd5223b26a1672edb8c4077cc5e11b6c27a8f4e6
andreammgcol <amendez72@misena.edu.co>
2019-10-13 23:09:39 +0000

    arguments

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,40 +13,24 @@ int cents, ncoins = 0, balance = 0;
 if (argc < 2)
 {
 	printf("Error\n");
-}
-else
-{
-	cents = atoi(argv[1]);
-	balance = cents;
-	while (balance > 0)
-	{
-		if (balance >= 25)
-		{
-			balance = balance - 25;
-			ncoins++;
-		}
-		else if (balance >= 10 && balance < 25)
-		{
-			balance = balance - 10;
-			ncoins++;
-		}
-		else if (balance >= 5 && balance < 10)
-		{
-			balance = balance - 5;
-			ncoins++;
-		}
-		else if (balance >= 2 && balance < 5)
-		{
-			balance = balance - 2;
-			ncoins++;
-		}
-		else if (balance == 1)
-		{
-			balance = balance - 1;
-			ncoins++;
-		}
-	}
-	printf("%d\n", ncoins);
-}
 	return (0);
+}
+cents = atoi(argv[1]);
+balance = cents;
+while (balance > 0)
+{
+	if (balance >= 25)
+		balance = balance - 25;
+	else if (balance >= 10 && balance < 25)
+		balance = balance - 10;
+	else if (balance >= 5 && balance < 10)
+		balance = balance - 5;
+	else if (balance >= 2 && balance < 5)
+		balance = balance - 2;
+	else if (balance == 1)
+		balance = balance - 1;
+	ncoins++;
+}
+printf("%d\n", ncoins);
+return (0);
 }
