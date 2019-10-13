81ab1d536c7b88f5f28c36db66fa68e02b7c285e
andreammgcol <amendez72@misena.edu.co>
2019-10-13 01:10:34 +0000

    arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,20 @@
 #include "holberton.h"
+/**
+* main - that prints its name
+* @argc: int argument count
+* @argv: char argument
+* Return: 0
+*/
 
 int main(int argc, char *argv[])
 {
 	int i = 0;
+
 	while (argv[argc - 1][i] != '\0')
 	{
 		_putchar(argv[argc - 1][i]);
 		i++;
 	}
 	_putchar('\n');
-	return 0;
+	return (0);
 }
