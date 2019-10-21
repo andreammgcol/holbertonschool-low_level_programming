bf49ed794c93d2d2623f3f409d412b0007dae885
Holberton <holberton@Holbertons-iMac-9.local>
2019-10-21 16:08:21 -0500

    structures, typedef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,11 @@
+# 0x0E. C - Structures, typedef
+
+---
+# General Objectives
+
+- What are structures, when, why and how to use them
+- How to use typedef
+
+---
+# Tasks
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef ANDREA_H
+#define ANDREA_H
+
+/**
+ * struct dog - Define a new type with Django
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
