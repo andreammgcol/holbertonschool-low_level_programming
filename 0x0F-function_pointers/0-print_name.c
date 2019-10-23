a09a8c28c64c2034d556f026a2aaa7379585edbe
Holberton <holberton@Holbertons-iMac-9.local>
2019-10-23 13:06:28 -0500

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+* print_name - that prints a name
+* @name: variable char
+* @f: pointer a function
+* Return:
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef FUNCTIONPOINTERS_H
+#define FUNCTIONPOINTERS_H
+void print_name(char *name, void (*f)(char *));
+
+#endif
