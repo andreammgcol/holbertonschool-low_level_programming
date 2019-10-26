420ffd88fdfcd7143b2dd54b8f3c715b253dddbf
Holberton <holberton@Holbertons-iMac-9.local>
2019-10-26 12:55:16 -0500

    variadic func

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - that prints strings
+ * @n: variable unsigned int
+ * @separator: variable pointer
+ *
+ * Return:
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list str;
+	unsigned int i;
+	//char *strtemp;
+
+
+	va_start(str, n);
+
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 1; i <= n; i++)
+	{
+		if (i == '\0')
+		{
+			printf("(nil)");
+		}
+
+		else if (i < n)
+		{
+			printf("%s%s", va_arg(str, char *), separator);
+		}
+
+		else
+		{
+			printf("%s", va_arg(str, char *));
+		}
+
+	}
+	printf("\n");
+	va_end(str);
+}
