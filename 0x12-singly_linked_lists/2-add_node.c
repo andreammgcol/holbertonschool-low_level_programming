bce3699a610fd39e1c2f1be050f7edd392f141d0
andreammgcol <amendez72@misena.edu.co>
2019-10-31 23:48:44 +0000

    task 2 singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,6 +14,11 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
