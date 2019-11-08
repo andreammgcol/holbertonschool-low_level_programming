c588ee4479b95c295a5107254049484f2a005ed7
andreammgcol <amendez72@misena.edu.co>
2019-11-08 01:02:58 +0000

    task 2 bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,13 +13,17 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= 64)
-	 	return (-1);
+		return (-1);
 
 	n = n >> index;
 
 	if ((n & 1) == 1)
 	{
 		return (1);
+	}
+	else
+	{
+	return (0);
 	}
 
 }
