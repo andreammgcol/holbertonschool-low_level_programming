ffc06088c9b4eee8f36aef2ec7d2fc9c985d7630
andreammgcol <amendez72@misena.edu.co>
2019-11-08 14:18:14 -0500

    task advanced

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* get_endianness - that checks the endianness
+*
+* Description: Write a function that checks the endianness.
+* Return: 0 if big endian, 1 if little endian
+*/
+
+int get_endianness(void)
+{
+	return (1);
+}
