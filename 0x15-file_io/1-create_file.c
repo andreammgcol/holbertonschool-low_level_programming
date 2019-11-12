bb134f4b2f98eee0062bf553daf9eddd4f43f796
andreammgcol <amendez72@misena.edu.co>
2019-11-12 17:17:06 -0500

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0, file, a;
+	int i = 0, file;
 
 	if (filename == NULL)
 		return (-1);
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-	a = write(file, text_content, i);
+	write(file, text_content, i);
 
 	return (1);
 }
