c1cdbbae2098fb85d7bb1f9e1f14c9b011af6801
andreammgcol <amendez72@misena.edu.co>
2019-11-12 17:06:54 -0500

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,12 +20,10 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	if (filename != NULL)
+
+	while (text_content[i] != '\0')
 	{
-		while (text_content[i] != '\0')
-		{
-			i++;
-		}
+		i++;
 	}
 
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
