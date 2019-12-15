96f51b48bff72cb0ce92fcc8ac5ce2969ccece0f
andreammgcol <amendez72@misena.edu.co>
2019-12-15 03:47:20 +0000

    task 6 sum nodes

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* sum_dlistint - that returns the sum of all the data (n) of a list
+* @head: variable pointer
+* Return: the sum of all the data (n)
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *i;
+	int sum = 0;
+
+	for (i = head; i != NULL; i = i->next)
+	{
+		sum = sum + i->n;
+	}
+	return (sum);
+}
+
